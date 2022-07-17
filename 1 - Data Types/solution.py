152d6d8afe08a1e7d178abc051daa8046bcb68da
Ricardo Serrato <serrato.ricardo97@gmail.com>
2022-07-16 21:15:00 -0500

    Added python solution for problem

---
+++ 1 - Data Types/solution.py
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+a_int = int(input())
+a_float = float(input())
+a_string = input()
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i + a_int)
+# Print the sum of the double variables on a new line.
+print(d + a_float)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + a_string)
